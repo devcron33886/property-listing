e9431cd53107ebe143e3df857779d9606d0e6e98
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-05-25 15:07:19 +0200

    Adding slug field source and image mauoilation

--- database/migrations/2022_05_25_000007_create_locations_table.php
+++ database/migrations/2022_05_25_000007_create_locations_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLoactionsTable extends Migration
+class CreateLocationsTable extends Migration
 {
     public function up()
     {
-        Schema::create('loactions', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('state');
             $table->timestamps();
